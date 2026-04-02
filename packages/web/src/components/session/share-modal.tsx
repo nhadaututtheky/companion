46de1aa8fda5f12d37ac530c3ec78f183510eaa1
nhadaututtheky <nguyenvietnam.it@gmail.com>
2026-04-02 15:10:40 +0700

    fix: suppress img warning in share-modal QR code (base64 data URL)

--- packages/web/src/components/session/share-modal.tsx
+++ packages/web/src/components/session/share-modal.tsx
@@ -214,6 +214,7 @@ export function ShareModal({ sessionId, onClose }: ShareModalProps) {
         {/* QR Code display */}
         {qrDataUrl && selectedToken && (
           <div className="flex flex-col items-center gap-3 py-2">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src={qrDataUrl}
               alt="QR code for session share link"
